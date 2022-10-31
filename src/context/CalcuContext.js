8921e06ad35ebdd06db296228ce47e45e67ba377
Joshua Owotogbe <owotogbesegunjoshua@gmail.com>
2022-10-31 18:39:05 +0100

    Add files via upload

---
+++ src/context/CalcuContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState} from "react";
+
+
+export const CalcuContext = createContext()
+const CalcProvider = ({ children }) => {
+const [calc, setCalc]= useState({
+    sign: "",
+    num: 0,
+    res: 0 
+})
+const providerValue = {
+    calc, setCalc
+}
+  return (
+  <CalcuContext.Provider value={providerValue}>
+    {children}
+    </CalcuContext.Provider>
+  )
+};
+
+export default CalcProvider;
